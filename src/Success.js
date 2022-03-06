08121194a55e8d880f10e997c7aa2cd1f82ee468
Baeju0 <young92745@naver.com>
2022-03-06 23:23:10 +0900

    Success창, * URL로 직접 접근 막기

---
+++ src/Success.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import './Success.scss';
+
+function Success() {
+    var [seconds,setSeconds] = useState(5);
+
+        useEffect(()=> {
+            setTimeout(()=> {
+                setSeconds(seconds-1)},1000);
+            },[seconds])
+
+        useEffect(()=> {
+          setTimeout(()=> {
+            window.location.replace("/login")},5000);
+        },[]);
+
+        return(
+        <div className="success">
+            <h2>회원가입 성공😊</h2>
+            <p>{seconds}초 후 로그인 페이지로 이동합니다...</p></div>
+    )
+}
+
+// URL로 접근 막기
+
+export default Success;
