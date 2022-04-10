fd2415e828f67a5e4d0e8af77c43bed877ba0fc3
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-04-10 21:13:56 +0900

    props retry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { ReactDOM } from 'react';
+import { ReactDOM, useState } from 'react';
 // import Weather from './Weather';
 import Radio from './Radio';
 import Select from './Select';
@@ -10,7 +10,9 @@ import LocalStorageComponent from './LocalStorageComponent';
 import Counter from './useCallback';
 import Propsss from './Props';
 
-function App() {
+function App(props) {
+  let [name, setName] = useState(['Juyoung','Window','Mac']);
+
   return (
     <div>
       {/* <Header/>
@@ -20,7 +22,8 @@ function App() {
     {/* <Select/> */}
     {/* <LocalStorageComponent/> */}
     {/* <Counter/> */}
-    <Propsss/>
+    <h1>안녕하세요!{name}</h1>
+    <App name={name}/>
     </div>
   );
 }
--- src/Props.js
+++ src/Props.js
@@ -1,16 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactDOM } from "react-dom";
 
 
 //props 문법
 function Propsss(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
   
-  const element = <Propsss name="Juyoung" />;
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
+    return <h1>Hello, {props.name[0]}</h1>;
 
+}
   export default Propsss;
